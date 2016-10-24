6a4241672548f5b25f6b71f9528914d1ca7915b2
Fab27 <montini.fabrizio@gmail.com>
2016-10-24 21:45:09 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+
+/* Using vanilla JavaScript */
+ 
+// Init ScrollMagic
+var ctrl = new ScrollMagic.Controller({
+    globalSceneOptions: {
+        triggerHook: 'onLeave',
+    }
+});
+
+// Create scene
+$("section").each(function() {
+ 
+    new ScrollMagic.Scene({
+        triggerElement: this, 
+    })
+    .setPin(this)
+    .addIndicators()
+    .addTo(ctrl);
+ 
+});
+
+$("section").each(function() {
+	new ScrollMagic.Scene({
+		triggerElement: this, 
+		duration: 350,
+	})
+	.setPin(this)
+	.addIndicators()
+    .addTo(ctrl);
+})
+
+
+$(window).on("resize", function(e) {
+  if ($(window).width() < 769 && scene.enabled()) {
+    scene.enabled(false);
+  } else if (!scene.enabled()) {
+    scene.enabled(true);
+  }
+});
